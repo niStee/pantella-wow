776b4411efb5318c4a473618f0045a19a0b78d8f
niStee <52573120+niStee@users.noreply.github.com>
2026-06-20 23:36:57 +0200

    refactor: conform to Pantella addon architecture

---
+++ character_db/wow_character_db.py
@@ -0,0 +1,8 @@
+from src.character_dbs.skyrim_character_db import SkyrimCharacterDB
+
+class WowCharacterDB(SkyrimCharacterDB):
+    """
+    World of Warcraft character DB.
+    Currently inherits from SkyrimCharacterDB and can be customized later.
+    """
+    pass
---
+++ character_generators/wow_character_generator.py
@@ -0,0 +1,8 @@
+from src.character_generators.skyrim_character_generator import SkyrimCharacterGenerator
+
+class WowCharacterGenerator(SkyrimCharacterGenerator):
+    """
+    World of Warcraft character generator.
+    Currently inherits from SkyrimCharacterGenerator and can be customized later.
+    """
+    pass
